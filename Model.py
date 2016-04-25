ac3437a83091e5db3d6ef300fa63a0e90e8094ab
Eric Rincon <eric.rincon147@gmail.com>
2016-04-25 17:07:10 -0500

    Removed text generation from every epoch

--- Model.py
+++ Model.py
@@ -22,32 +22,32 @@ class Sequence2Sequence(object):
 
             self.model.fit(X, y, batch_size=128, nb_epoch=1)
 
-            start_index = random.randint(0, len(text) - maxlen - 1)
+        start_index = random.randint(0, len(text) - maxlen - 1)
 
-            for diversity in [0.2, 0.5, 1.0, 1.2]:
-                print()
-                print('----- diversity:', diversity)
+        for diversity in [0.2, 0.5, 1.0, 1.2]:
+            print()
+            print('----- diversity:', diversity)
 
-                generated = ''
-                sentence = text[start_index: start_index + maxlen]
-                generated += sentence
-                print('----- Generating with seed: "' + sentence + '"')
-                sys.stdout.write(generated)
+            generated = ''
+            sentence = text[start_index: start_index + maxlen]
+            generated += sentence
+            print('----- Generating with seed: "' + sentence + '"')
+            sys.stdout.write(generated)
 
-                for i in range(400):
-                    x = np.zeros((1, maxlen, len(chars)))
-                    for t, char in enumerate(sentence):
-                        x[0, t, char_indices[char]] = 1.
+            for i in range(400):
+                x = np.zeros((1, maxlen, len(chars)))
+                for t, char in enumerate(sentence):
+                    x[0, t, char_indices[char]] = 1.
 
-                    preds = self.model.predict(x, verbose=0)[0]
-                    next_index = self.sample(preds, diversity)
-                    next_char = indices_char[next_index]
+                preds = self.model.predict(x, verbose=0)[0]
+                next_index = self.sample(preds, diversity)
+                next_char = indices_char[next_index]
 
-                    generated += next_char
-                    sentence = sentence[1:] + next_char
+                generated += next_char
+                sentence = sentence[1:] + next_char
 
-                    sys.stdout.write(next_char)
-                    sys.stdout.flush()
+                sys.stdout.write(next_char)
+                sys.stdout.flush()
 
 
 
